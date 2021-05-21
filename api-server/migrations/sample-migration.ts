99b3e332d81d7f2e134d392decd07966d308bcec
zacmonroe <zmonroe0820@gmail.com>
2021-05-21 14:38:57 -0400

    added migrations capability

---
+++ api-server/migrations/sample-migration.ts
@@ -0,0 +1,9 @@
+import { Db } from "mongodb";
+
+// Need to use module.exports to work with migrate-mongo
+module.exports = {
+	async up(db: Db) {
+	},
+	async down(db: Db) {
+	},
+}
--- api-server/scripts/gen-hmac.ts
+++ api-server/scripts/gen-hmac.ts
@@ -1,4 +1,4 @@
 import crypto from "crypto";
 
 const randomString = crypto.randomBytes(32).toString("hex")
-console.log(`export const hmacKey = "${randomString}";`);
+console.log(`export const hmacKey = "${randomString}";\n`);
